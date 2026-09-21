a86adaf9c59cda42986f62443da7a3b2c9662fb1
Amlal El Mahrouss <amlal@nekernel.org>
2026-09-21 15:29:35 +0200

    FEAT: src: specify driver type in comments for NectarCompiler AMD64 and PTX

--- private/src/CompilerKit/src/Frontends/NectarCompiler+PTX.cpp
+++ private/src/CompilerKit/src/Frontends/NectarCompiler+PTX.cpp
@@ -12,7 +12,7 @@
 #include <CompilerKit/UUID.h>
 #include <CompilerKit/Utils/Compiler.h>
 
-/* Nectar Compiler Driver. */
+/* Nectar Compiler Driver for Nvidia PTX. */
 /* This is part of the CompilerKit. */
 /* (c) Amlal El Mahrouss 2024-2026 */
 
